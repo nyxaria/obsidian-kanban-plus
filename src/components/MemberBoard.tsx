b81da0f9aa8abc25fe3641f083215be649bfd0d3
George Hartt <gh503@cam.ac.uk>
2025-06-16 20:24:56 +0100

    working member view v1

--- src/components/MemberBoard.tsx
+++ src/components/MemberBoard.tsx
@@ -2,237 +2,37 @@ import classcat from 'classcat';
 import { TFile } from 'obsidian';
 import { Component, memo } from 'preact/compat';
 import { useCallback, useContext, useEffect, useRef, useState } from 'preact/hooks';
+import { ScrollContainer } from 'src/dnd/components/ScrollContainer';
+import { SortPlaceholder } from 'src/dnd/components/SortPlaceholder';
+import { Sortable } from 'src/dnd/components/Sortable';
 
 import { MemberView } from '../MemberView';
 import { DndContext } from '../dnd/components/DndContext';
 import { Droppable } from '../dnd/components/Droppable';
 import { DndManagerContext } from '../dnd/components/context';
-import { useDragHandle } from '../dnd/managers/DragManager';
 import { getBoardModifiers } from '../helpers/boardModifiers';
 import { t } from '../lang/helpers';
 import KanbanPlugin from '../main';
-import { DateAndTime, RelativeDate } from './Item/DateAndTime';
 import { Items } from './Item/Item';
-import { ItemCheckbox } from './Item/ItemCheckbox';
-import { AssignedMembers, ItemContent, Tags } from './Item/ItemContent';
-import { ItemPriority } from './Item/ItemPriority';
-import { ItemMetadata } from './Item/MetadataTable';
-import { getItemClassModifiers } from './Item/helpers';
 import { KanbanContext, SearchContext } from './context';
-import { c, useGetDateColorFn } from './helpers';
-import {
-  Board,
-  EditState,
-  EditingProcessState,
-  Item,
-  Lane,
-  TeamMemberColorConfig,
-  isEditingActive,
-} from './types';
+import { c } from './helpers';
+import { Board, DataTypes, Item, Lane } from './types';
 
-// Custom DraggableItem that properly handles the isStatic prop for drag binding
-interface CustomDraggableItemProps {
-  item: Item;
-  itemIndex: number;
-  laneId: string;
-  shouldMarkItemsComplete: boolean;
-  cancelEditCounter: number;
-  isStatic?: boolean;
-}
-
-const CustomDraggableItem = memo(function CustomDraggableItem(props: CustomDraggableItemProps) {
-  const elementRef = useRef<HTMLDivElement>(null);
-  const measureRef = useRef<HTMLDivElement>(null);
-  const search = useContext(SearchContext);
-  const { stateManager, boardModifiers, filePath, view } = useContext(KanbanContext);
+// Debug component to check DndManager context
+function DndDebugInfo() {
   const dndManager = useContext(DndManagerContext);
-  const [editState, setEditState] = useState<EditState>(EditingProcessState.Cancel);
-  const [isHovered, setIsHovered] = useState(false);
 
-  const { itemIndex, laneId, isStatic, ...innerProps } = props;
+  useEffect(() => {
+    console.log('[MemberBoard] DndDebugInfo - DndManager context:', {
+      hasDndManager: !!dndManager,
+      dndManagerType: typeof dndManager,
+      dragManager: dndManager?.dragManager ? 'present' : 'missing',
+      dropManager: dndManager?.dropManager ? 'present' : 'missing',
+    });
+  }, [dndManager]);
 
-  console.log('[CustomDraggableItem] Rendering item:', {
-    itemId: props.item.id,
-    itemTitle: props.item.data.title,
-    isStatic,
-    laneId,
-    itemIndex,
-    hasDndManager: !!dndManager,
-    dndManagerType: typeof dndManager,
-  });
-
-  // Import drag handle hook but only use it when not static
-  const bindHandle = isStatic ? () => {} : useDragHandle(measureRef, measureRef);
-
-  console.log('[CustomDraggableItem] bindHandle created:', {
-    itemId: props.item.id,
-    isStatic,
-    bindHandleType: typeof bindHandle,
-  });
-
-  const getDateColor = useGetDateColorFn(stateManager);
-  const teamMemberColors: Record<string, TeamMemberColorConfig> =
-    view.plugin.settings.teamMemberColors || {};
-
-  const isMatch = search?.query ? innerProps.item.data.titleSearch.includes(search.query) : false;
-  const classModifiers: string[] = getItemClassModifiers(innerProps.item);
-
-  // Static items are read-only, no editing allowed
-  const onDoubleClickCallback = useCallback(() => {
-    if (isStatic) return; // No editing for static items
-    setEditState({ x: 0, y: 0 });
-  }, [isStatic]);
-
-  const onContextMenu = useCallback(() => {
-    if (isStatic) return; // No context menu for static items
-  }, [isStatic]);
-
-  return (
-    <div
-      ref={(el) => {
-        console.log('[CustomDraggableItem] Setting ref for item:', {
-          itemId: props.item.id,
-          element: el,
-          isStatic,
-          willBindHandle: !isStatic,
-        });
-        measureRef.current = el;
-        if (!isStatic) {
-          console.log('[CustomDraggableItem] Calling bindHandle for item:', props.item.id);
-          bindHandle(el);
-        } else {
-          console.log(
-            '[CustomDraggableItem] Skipping bindHandle (static) for item:',
-            props.item.id
-          );
-        }
-      }}
-      className={c('item-wrapper')}
-    >
-      <div
-        ref={elementRef}
-        className={classcat([c('item'), ...classModifiers])}
-        data-id={props.item.id}
-        onMouseEnter={() => {
-          console.log('[CustomDraggableItem] Mouse enter:', props.item.id);
-          setIsHovered(true);
-        }}
-        onMouseLeave={() => {
-          console.log('[CustomDraggableItem] Mouse leave:', props.item.id);
-          setIsHovered(false);
-        }}
-        onDoubleClick={onDoubleClickCallback}
-        onContextMenu={onContextMenu}
-        onPointerDown={(e) => {
-          console.log('[CustomDraggableItem] Pointer down on item:', {
-            itemId: props.item.id,
-            isStatic,
-            target: e.target,
-            currentTarget: e.currentTarget,
-          });
-        }}
-        onDragStart={(e) => {
-          console.log('[CustomDraggableItem] Drag start on item:', {
-            itemId: props.item.id,
-            isStatic,
-            event: e,
-          });
-        }}
-      >
-        {isStatic ? (
-          // Static version - no Droppable wrapper
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              marginBottom: '4px',
-            }}
-          >
-            <ItemCheckbox
-              isVisible={false} // Never show checkbox for static items
-              item={innerProps.item}
-              path={[itemIndex]}
-              shouldMarkItemsComplete={innerProps.shouldMarkItemsComplete}
-              stateManager={stateManager}
-              boardModifiers={boardModifiers}
-              style={{ marginRight: '4px', marginLeft: '8px' }}
-            />
-            <div style={{ flexGrow: 1, minWidth: 0 }}>
-              <ItemContent
-                item={innerProps.item}
-                editState={editState}
-                setEditState={setEditState}
-                searchQuery={isMatch ? search?.query : undefined}
-                isStatic={true}
-                style={{ marginLeft: '-2.5px', marginRight: '0px' }}
-              />
-            </div>
-          </div>
-        ) : (
-          // Dynamic version - with Droppable wrapper for drag-and-drop
-          <Droppable
-            elementRef={elementRef}
-            measureRef={measureRef}
-            id={props.item.id}
-            index={itemIndex}
-            data={{
-              id: props.item.id,
-              type: 'item',
-              accepts: ['item'],
-              parentLaneId: laneId,
-              originalIndex: itemIndex,
-            }}
-          >
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'flex-start',
-                marginBottom: '4px',
-              }}
-            >
-              <ItemCheckbox
-                isVisible={isHovered && !isEditingActive(editState)}
-                item={innerProps.item}
-                path={[itemIndex]}
-                shouldMarkItemsComplete={innerProps.shouldMarkItemsComplete}
-                stateManager={stateManager}
-                boardModifiers={boardModifiers}
-                style={{ marginRight: '4px', marginLeft: '8px' }}
-              />
-              <div style={{ flexGrow: 1, minWidth: 0 }}>
-                <ItemContent
-                  item={innerProps.item}
-                  editState={editState}
-                  setEditState={setEditState}
-                  searchQuery={isMatch ? search?.query : undefined}
-                  isStatic={false}
-                  style={{ marginLeft: '-2.5px', marginRight: '0px' }}
-                />
-              </div>
-            </div>
-          </Droppable>
-        )}
-
-        {/* Item metadata and other components */}
-        <div style={{ marginLeft: '8px', marginRight: '8px' }}>
-          <Tags item={innerProps.item} />
-          <AssignedMembers item={innerProps.item} teamMemberColors={teamMemberColors} />
-          <ItemPriority item={innerProps.item} />
-
-          {innerProps.item.data.metadata.date && (
-            <DateAndTime
-              item={innerProps.item}
-              stateManager={stateManager}
-              getDateColor={getDateColor}
-            />
-          )}
-
-          <ItemMetadata item={innerProps.item} />
-        </div>
-      </div>
-    </div>
-  );
-});
+  return null;
+}
 
 interface MemberBoardProps {
   plugin: KanbanPlugin;
@@ -437,9 +237,17 @@ export function MemberBoard(props: MemberBoardProps) {
   const searchContext = {
     query: '',
     items: new Set(),
-    lanes: new Set(),
-    search: () => {},
   };
+
+  console.log('[MemberBoard] Context setup:', {
+    hasKanbanContext: !!kanbanContext,
+    hasStateManager: !!kanbanContext.stateManager,
+    hasBoardModifiers: !!kanbanContext.boardModifiers,
+    hasSearchContext: !!searchContext,
+    filePath: kanbanContext.filePath,
+    boardId: board?.id,
+    laneCount: board?.children?.length,
+  });
 
   // Safety check for required props
   if (!plugin || !view || !board) {
@@ -479,6 +287,8 @@ export function MemberBoard(props: MemberBoardProps) {
     },
     [selectedMember, onMemberChange]
   );
+
+  const laneAccepts = [DataTypes.Item];
 
   return (
     <div className={c('member-board-container')}>
@@ -571,70 +381,73 @@ export function MemberBoard(props: MemberBoardProps) {
                   console.log('[MemberBoard] DndContext onDrop called:', {
                     dragEntity,
                     dropEntity,
+                    dragData: dragEntity?.getData(),
+                    dropData: dropEntity?.getData(),
                   });
                   view.handleDrop(dragEntity, dropEntity);
                 }}
               >
                 <div className={c('board')}>
                   <div className={c('lanes')}>
-                    {board.children.map((lane, laneIndex) => (
-                      <div
-                        key={lane.id}
-                        className={c('lane-wrapper')}
-                        style={{ width: '272px' }}
-                        onDragOver={(e) => e.preventDefault()}
-                        onDrop={(e) => {
-                          e.preventDefault();
-                          try {
-                            const dragData = JSON.parse(e.dataTransfer.getData('text/plain'));
-                            if (dragData.type === 'item') {
-                              // Create mock drop entity for handleDrop
-                              const dropEntity = {
-                                getData: () => ({
-                                  type: 'lane',
-                                  id: lane.id,
-                                  index: lane.children.length,
-                                }),
-                                getPath: () => [laneIndex, lane.children.length],
-                              };
-                              const dragEntity = {
-                                getData: () => dragData,
-                              };
-                              view.handleDrop(dragEntity, dropEntity);
-                            }
-                          } catch (error) {
-                            console.warn('[MemberBoard] Drop error:', error);
-                          }
-                        }}
-                      >
-                        <div className={c('lane')}>
-                          <div className={c('lane-header-wrapper')}>
-                            <div className={c('lane-title')}>
-                              <div className={c('lane-title-text')}>{lane.data.title}</div>
-                              <div className={c('lane-title-count')}>{lane.children.length}</div>
+                    {board.children.map((lane, laneIndex) => {
+                      const laneRef = useRef<HTMLDivElement>(null);
+
+                      console.log('[MemberBoard] Rendering lane:', {
+                        laneId: lane.id,
+                        laneIndex,
+                        laneTitle: lane.data.title,
+                        itemCount: lane.children.length,
+                        hasLaneRef: !!laneRef,
+                      });
+
+                      return (
+                        <div key={lane.id} className={c('lane-wrapper')} style={{ width: '272px' }}>
+                          <div ref={laneRef} className={c('lane')}>
+                            <div className={c('lane-header-wrapper')}>
+                              <div className={c('lane-title')}>
+                                <div className={c('lane-title-text')}>{lane.data.title}</div>
+                                <div className={c('lane-title-count')}>{lane.children.length}</div>
+                              </div>
+                            </div>
+                            <div className={c('lane-items-wrapper')}>
+                              <Droppable
+                                elementRef={laneRef}
+                                measureRef={laneRef}
+                                id={lane.id}
+                                index={laneIndex}
+                                data={lane}
+                              >
+                                <KanbanContext.Provider value={kanbanContext}>
+                                  <SearchContext.Provider value={searchContext}>
+                                    <DndDebugInfo />
+                                    <ScrollContainer
+                                      className="member-board-lane-content"
+                                      id={lane.id}
+                                      index={laneIndex}
+                                      triggerTypes={laneAccepts}
+                                    >
+                                      <Sortable onSortChange={() => {}} axis="vertical">
+                                        <Items
+                                          items={lane.children}
+                                          laneId={lane.id}
+                                          shouldMarkItemsComplete={lane.id === 'done'}
+                                          targetHighlight={null}
+                                          cancelEditCounter={0}
+                                        />
+                                        <SortPlaceholder
+                                          accepts={laneAccepts}
+                                          index={lane.children.length}
+                                        />
+                                      </Sortable>
+                                    </ScrollContainer>
+                                  </SearchContext.Provider>
+                                </KanbanContext.Provider>
+                              </Droppable>
                             </div>
                           </div>
-                          <div className={c('lane-items-wrapper')}>
-                            <KanbanContext.Provider value={kanbanContext}>
-                              <SearchContext.Provider value={searchContext}>
-                                {/* Custom draggable items with proper static handling */}
-                                {lane.children.map((item, i) => (
-                                  <CustomDraggableItem
-                                    key={item.id}
-                                    item={item}
-                                    itemIndex={i}
-                                    laneId={lane.id}
-                                    shouldMarkItemsComplete={false}
-                                    cancelEditCounter={0}
-                                    isStatic={false}
-                                  />
-                                ))}
-                              </SearchContext.Provider>
-                            </KanbanContext.Provider>
-                          </div>
                         </div>
-                      </div>
-                    ))}
+                      );
+                    })}
                   </div>
                 </div>
               </DndContext>
